28426a2bbd97bc21138718cac66cbc429a1f6ca4
PhillipKellogg <kelloggphillip@gmail.com>
2020-08-10 12:43:40 -0400

    New libraries

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const app = express();
-const Easy = require('./Easy');
-const Hard = require('./Hard');
-const Medium = require('./Medium');
+require("dotenv").config();
 
-const cors = require('cors');
+const express = require("express");
+const app = express();
+const Easy = require("./Easy");
+const Hard = require("./Hard");
+const Medium = require("./Medium");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-
-app.use("/", Easy)
-app.use("/hard", Hard)
-app.use("/medium", Medium)
-
+app.use("/", Easy);
+app.use("/hard", Hard);
+app.use("/medium", Medium);
 
 app.listen(8000, () => {
-    console.log("Hello Everyone!")
-})
+  console.log("Hello Everyone!");
+});
